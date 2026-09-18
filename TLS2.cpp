a38105fb7a2b3c95aa75b86fc435864e8b232761
tangguhputra16-beep <tangguh.putra16@gmail.com>
2026-09-18 21:44:29 +0700

    Add files via upload

---
+++ TLS2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char encrypted[100];
+
+    cout << "Encrypted text: ";
+    cin.getline(encrypted, 100);
+
+    int i = 0;
+    int sebelumnya;
+
+    while (encrypted[i] != '\0') {
+
+        if (i == 0) {
+            cout << encrypted[i];
+
+            sebelumnya = encrypted[i] - 'A' + 1;
+        }
+        else {
+            int sekarang = encrypted[i] - 'A' + 1;
+
+            int hasil = sekarang - sebelumnya;
+
+            if (hasil <= 0) {
+                hasil = hasil + 26;
+            }
+
+            char huruf = 'A' + hasil - 1;
+
+            cout << huruf;
+
+            // Simpan HURUF ASLI yang baru didapat
+            sebelumnya = hasil;
+        }
+
+        i++;
+    }
+
+    cout << endl;
+
+    return 0;
+}
